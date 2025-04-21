4d76bdcb0fff4c931f680b9f701326bc0c8610f0
Jeffrey <jeffreycdenton@gmail.com>
2025-04-21 00:35:40 -0700

    correct app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage    from './HomePage';
 import SignUp      from './pages/SignUp';
 import LogIn       from './pages/LogIn';
